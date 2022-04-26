2079e1fe306eebc52f71db0a2e4807141d886f57
Omar Ashour <omar.a.ashour@gmail.com>
2022-04-26 17:17:13 +0200

    added styles to features

--- dist/main.ca7b36d6.js
+++ dist/main.ca7b36d6.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./../assets/images/bg-curvy-mobile.svg":[["bg-curvy-mobile.446062bd.svg","assets/images/bg-curvy-mobile.svg"],"assets/images/bg-curvy-mobile.svg"],"./../assets/images/bg-quotes.png":[["bg-quotes.90015005.png","assets/images/bg-quotes.png"],"assets/images/bg-quotes.png"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../assets/images/bg-curvy-mobile.svg":[["bg-curvy-mobile.446062bd.svg","assets/images/bg-curvy-mobile.svg"],"assets/images/bg-curvy-mobile.svg"],"./../assets/images/bg-quotes.png":[["bg-quotes.90015005.png","assets/images/bg-quotes.png"],"assets/images/bg-quotes.png"],"./../assets/images/bg-curvy-desktop.svg":[["bg-curvy-desktop.d38e5f9c.svg","assets/images/bg-curvy-desktop.svg"],"assets/images/bg-curvy-desktop.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
