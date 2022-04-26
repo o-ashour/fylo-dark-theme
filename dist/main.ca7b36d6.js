09c8e8ea872233798be704041e28812b83064e96
Omar Ashour <omar.a.ashour@gmail.com>
2022-04-26 12:10:46 +0200

    removed about page

--- dist/main.ca7b36d6.js
+++ dist/main.ca7b36d6.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50260" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50749" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
